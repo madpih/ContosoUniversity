c4a61530804da21ca4dcb9c7f79322a60764f203
madis <madis.pihlasalu@gmail.com>
2023-09-17 21:50:55 +0300

    fixing posting error/changing some properties to nullable in models

--- ContosoUniversity/Models/Department.cs
+++ ContosoUniversity/Models/Department.cs
@@ -23,7 +23,7 @@ namespace ContosoUniversity.Models
 
         public int? InstructorID { get; set; }
 
-        public Instructor Administrator { get; set; }
-        public ICollection<Course> Courses { get; set; }
+        public Instructor? Administrator { get; set; }
+        public ICollection<Course>? Courses { get; set; }
     }
 }
--- ContosoUniversity/Models/SchoolViewModels/InstructorIndexData.cs
+++ ContosoUniversity/Models/SchoolViewModels/InstructorIndexData.cs
@@ -7,8 +7,8 @@ namespace ContosoUniversity.Models.SchoolViewModels
 {
     public class InstructorIndexData
     {
-        public IEnumerable<Instructor> Instructors { get; set; }
-        public IEnumerable<Course> Courses { get; set; }
+        public IEnumerable<Instructor>? Instructors { get; set; }
+        public IEnumerable<Course>? Courses { get; set; }
         public IEnumerable<Enrollment>? Enrollments { get; set; }
     }
 }
